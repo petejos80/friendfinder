9a50e73e6bdd6f2bfc31c316bc0e95d60300ef31
petejos80 <petejos80@gmail.com>
2018-05-11 18:03:28 -0500

    Added additional users

--- app/friends.js
+++ app/friends.js
@@ -7,10 +7,31 @@
 
 var friendsData = [
     {
-      customerName: "Ahmed",
-      customerEmail: "ahmed@example.com",
-      customerID: "afhaque89",
-      phoneNumber: "000-000-0000"
+      friendName: "Billy Anderson",
+      friendPhoto: "https://gadgetstouse.com/wp-content/uploads/2017/11/Xiaomi-Redmi-Y1-selfie-sample-daylight.jpg",
+      response1: "4",
+      response2: "3",
+      response3: "3",
+      response4: "3",
+      response5: "3",
+    },
+    {
+      friendName: "Sam Johnson",
+      friendPhoto: "https://i.redd.it/mgw8nj4naw1z.png",
+      response1: "4",
+      response2: "2",
+      response3: "4",
+      response4: "5",
+      response5: "3",
+    },
+    {
+      friendName: "Craig Ferguson",
+      friendPhoto: "https://unbox-seohacker.netdna-ssl.com/wp-content/uploads/2017/08/ASUS-ZenFone-4-Selfie-26.jpg",
+      response1: "4",
+      response2: "3",
+      response3: "2",
+      response4: "4",
+      response5: "3",
     }
   ];
   
