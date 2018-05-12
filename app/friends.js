edaefb681f685c3b01dc673db7d2776b10355711
petejos80 <petejos80@gmail.com>
2018-05-11 21:25:21 -0500

    Added array of numbers

--- app/friends.js
+++ app/friends.js
@@ -9,30 +9,36 @@ var friendsData = [
     {
       friendName: "Billy Anderson",
       friendPhoto: "https://gadgetstouse.com/wp-content/uploads/2017/11/Xiaomi-Redmi-Y1-selfie-sample-daylight.jpg",
-      response1: "4",
-      response2: "3",
-      response3: "3",
-      response4: "3",
-      response5: "3",
+      scores:[
+        1,
+        2,
+        5,
+        3,
+        3,
+      ]
     },
     {
       friendName: "Sam Johnson",
       friendPhoto: "https://i.redd.it/mgw8nj4naw1z.png",
-      response1: "4",
-      response2: "2",
-      response3: "4",
-      response4: "5",
-      response5: "3",
+      scores:[
+        1,
+        5,
+        5,
+        5,
+        3,
+      ]
     },
     {
       friendName: "Craig Ferguson",
-      friendPhoto: "https://unbox-seohacker.netdna-ssl.com/wp-content/uploads/2017/08/ASUS-ZenFone-4-Selfie-26.jpg",
-      response1: "4",
-      response2: "3",
-      response3: "2",
-      response4: "4",
-      response5: "3",
-    }
+      friendPhoto: "https://cdni.rbth.com/rbthmedia/images/all/2017/05/28/russia_selfie.jpg",
+      scores:[
+        4,
+        4,
+        4,
+        4,
+        4,
+      ]
+    },
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
