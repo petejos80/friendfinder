67578db14728fb8270674d6755aa63a7797223a9
petejos80 <petejos80@gmail.com>
2018-05-13 00:39:54 -0500

    First draft

--- app/friends.js
+++ app/friends.js
@@ -10,34 +10,34 @@ var friendsData = [
       friendName: "Billy Anderson",
       friendPhoto: "https://gadgetstouse.com/wp-content/uploads/2017/11/Xiaomi-Redmi-Y1-selfie-sample-daylight.jpg",
       scores:[
-        1,
-        2,
-        5,
-        3,
-        3,
-      ]
+        "1",
+        "2",
+        "5",
+        "3",
+        "3",
+      ],
     },
     {
       friendName: "Sam Johnson",
       friendPhoto: "https://i.redd.it/mgw8nj4naw1z.png",
       scores:[
-        1,
-        5,
-        5,
-        5,
-        3,
-      ]
+        "1",
+        "5",
+        "5",
+        "5",
+        "3",
+      ],
     },
     {
       friendName: "Craig Ferguson",
       friendPhoto: "https://cdni.rbth.com/rbthmedia/images/all/2017/05/28/russia_selfie.jpg",
       scores:[
-        4,
-        4,
-        4,
-        4,
-        4,
-      ]
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+      ],
     },
   ];
   
